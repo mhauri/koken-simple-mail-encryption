636f9c19873d55bc42f6903de770f2ab88c35e21
Marcel Hauri <marcel@hauri.me>
2015-04-18 13:04:01 +0200

    simplify code, make it work with ajax pages

--- README.md
+++ README.md
@@ -3,14 +3,20 @@ Simple Mail Encryption plugin for Koken
 
 This [Koken](http://koken.me) plugin automatically encrypts all plain text mail addresses in your site and make them clickable with a mailto: link.
 
+Facts
+-----
+- version: 0.2.0
+- works also with Madison 2 Theme
+
 Requirements
 ------------
-
-[Koken](http://koken.me) installation (0.2.0 or higher)
+- [Koken](http://koken.me) installation (0.2.0 or higher)
+- PHP >= 5.2.0
 
 Installation
 ------------
-
 1. Upload the koken-simple-mail-encryption folder to your Koken installation's storage/plugins directory.
 
 2. Sign in to Koken, then visit the Settings > Plugins page to activate the plugin.
+
+3. write your e-mail address as plain text in your page or essay, it will automatically be linked and protected by javascript
--- plugin.php
+++ plugin.php
@@ -1,10 +1,20 @@
 <?php
+/**
+ * koken-simple-mail-encryption is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This script is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * PHP version 5
+ *
+ * @author    Marcel Hauri <marcel@hauri.me>
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ */
 
 class KokenSimpleMailEncryption extends KokenPlugin {
-
-    protected $addresses = array();
-    protected $encrypted = array();
-    protected $vars = array();
 
     public function __construct()
     {
@@ -17,137 +27,37 @@ class KokenSimpleMailEncryption extends KokenPlugin {
      */
     public function render($output)
     {
+        $addresses = array();
+
         // Search for all email addresses in the output
-        preg_match_all('/[A-Za-z0-9_-]+@[A-Za-z0-9_-]+\.([A-Za-z0-9_-][A-Za-z0-9_]+)/', $output, $matches, PREG_PATTERN_ORDER);
+        $pattern = '/[a-z0-9_.\-\+]{1,256}+@[a-z0-9\-\.]+\.([a-z]{2,4})/i';
+        preg_match_all($pattern, $output, $matches, PREG_PATTERN_ORDER);
+
         foreach($matches['0'] as $match) {
-            $this->setAddress($match);
+            $split = explode('@', $match);
+            foreach($split as $part) {
+                $addresses[$match][] = $part;
+            }
         }
+
         // Check if there are any plain text mail addresses
-        if($this->getAddressesCount() > 0) {
-            // Encrypt email addresses
-            $this->encryptEmails();
+        if($addresses > 0) {
 
             // Replace amail addresses with encrypted part
-            $output = $this->replaceEmailAddresses($output);
-
-            // Add JS part at the end
-            $output = str_replace('</body>', $this->emailJS() . '</body>', $output);
+            $output = $this->replaceEmailAddresses($addresses, $output);
         }
         return $output;
     }
 
     /**
-     * @return string
-     */
-    public function emailJS()
-    {
-        $output = "<script type=\"text/javascript\">";
-        $output .= "var ";
-        $vars = "";
-        foreach ($this->getVars() as $key => $var) {
-            $vars .= $key .' = "' . $var . '", ';
-        }
-        $output .= substr($vars, 0, -2) . ';';
-        foreach ($this->getEncrypted() as $enc) {
-            $key = array_keys($enc['parts']);
-            $output .= "\n$('." . $enc['id'] . "').each(function() { $(this).html('<a href=\"mailto:' + " . $key['0'] . " + '@' + " . $key['1'] . " + '\">' + " . $key['0'] . " + '@' + " . $key['1'] . " + '</a>');});";
-        }
-        $output .= "</script>\n";
-        return $output;
-
-    }
-
-    /**
-     * @return array
-     */
-    public function getEncrypted()
-    {
-        return $this->encrypted;
-    }
-
-    /**
-     * @return array
-     */
-    public function getVars()
-    {
-        return $this->vars;
-    }
-
-    /**
-     * @return int
-     */
-    public function getAddressesCount()
-    {
-        return count($this->addresses);
-    }
-
-    /**
-     * @param $address
-     */
-    protected function setAddress($address)
-    {
-        $this->addresses[] = $address;
-    }
-
-    /**
-     * @param $var
-     */
-    protected function setVar($key, $value)
-    {
-        $this->vars[$key] = $value;
-    }
-
-    /**
-     * @param $key
-     * @param $content
-     */
-    protected function setEncrypted($key, $content)
-    {
-        if(is_array($content)) {
-            $this->encrypted[$key] = $content;
-        } else {
-            $this->encrypted[$key] = array($content);
-        }
-
-    }
-
-    /**
-     * encrypt mail addresses
-     */
-    protected function encryptEmails()
-    {
-        $addresses = $this->addresses;
-        foreach($addresses as $address) {
-            $parts = $this->splitAddressParts($address);
-            $this->setEncrypted($address, array('id' => 'mail_' . substr(sha1($address), 0, 6), 'parts' => $parts));
-        }
-    }
-
-    /**
-     * @param $address
-     * @return array
-     */
-    protected function splitAddressParts($address)
-    {
-        $output = array();
-        $split = explode('@', $address);
-        foreach($split as $part) {
-            $key = 'enc_' . substr(sha1($part), 0, 6);
-            $output[$key] = $part;
-            $this->setVar($key, $part);
-        }
-        return $output;
-    }
-
-    /**
+     * @param $addresses
      * @param $output
      * @return mixed
      */
-    protected function replaceEmailAddresses($output)
+    protected function replaceEmailAddresses($addresses, $output)
     {
-        $addresses = $this->getEncrypted();
-        foreach($addresses as $address => $parts) {
-            $replace = '<span class="' . $parts['id'] . '"></span>';
+        foreach($addresses as $address => $part) {
+            $replace = "<script type=\"text/javascript\">document.write('<a href=\"mailto:' + '" . $part[0] . "' + '@' + '" . $part[1] . "' + '\">' + '" . $part[0] . "' + '@' + '" . $part[1] . "' + '</a>');</script>";
             $output = str_replace($address, $replace, $output);
         }
         return $output;
